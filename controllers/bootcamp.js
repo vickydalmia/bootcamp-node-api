fa933198630f51aa561b1e54377d4a0fcff66c78
Vicky Dalmia <vicky8394@gmail.com>
2020-02-02 14:39:37 +0530

    CRUD boot camp implemented

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,26 +1,54 @@
 const bootcampService = require("../services/bootcamp");
 exports.getAll = async (req, res, next) => {
-  console.log(req.body);
-  const bootcamp = await bootcampService.createBootCamp(req.body);
-  console.log(bootcamp);
-  res.status(200).json({ name: "bootcamp" });
+  try {
+    const bootcamp = await bootcampService.getAllBootCamp();
+    res.status(200).json({ result: "success", data: bootcamp });
+  } catch (error) {
+    res.status(200).json({ result: "failure", msg: error.message });
+  }
 };
 
-exports.getOne = (req, res, next) => {
-  res.status(200).json({ id: `${req.params.id}` });
+exports.getOne = async (req, res, next) => {
+  try {
+    const bootcamp = await bootcampService.getBootCampById(req.params.id);
+    if (!bootcamp) {
+      return res
+        .status(404)
+        .json({ result: "failure", msg: "No Records Found" });
+    }
+    res.status(200).json({ result: "success", data: bootcamp });
+  } catch (error) {
+    res.status(200).json({ result: "failure", msg: error.message });
+  }
 };
 
-exports.update = (req, res, next) => {
-  res.status(200).json({ id: `${req.params.id}` });
+exports.update = async (req, res, next) => {
+  try {
+    const bootcamp = await bootcampService.updateBootCamp(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json({ result: "success", data: bootcamp });
+  } catch (error) {
+    res.status(200).json({ result: "failure", msg: error.message });
+  }
 };
 
-exports.deleteOne = (req, res, next) => {
-  res.status(200).json({ id: `${req.params.id}` });
+exports.deleteOne = async (req, res, next) => {
+  try {
+    const deletedId = await bootcampService.deleteBootCamp(req.params.id);
+    res.status(200).json({ result: "success", deletedId });
+  } catch (error) {
+    res.status(200).json({ result: "failure", msg: error.message });
+  }
 };
 
 exports.create = async (req, res, next) => {
-  console.log(req.body);
-  const bootcamp = await bootcampService.createBootCamp(req.body);
-  console.log(bootcamp);
-  res.status(200).json({ name: "bootcamp" });
+  try {
+    const bootcamp = await bootcampService.createBootCamp(req.body);
+    res.status(200).json({ data: bootcamp });
+  } catch (e) {
+    console.log(e);
+    res.status(200).json({ result: "failure", error: { msg: e.message } });
+  }
 };
--- services/bootcamp.js
+++ services/bootcamp.js
@@ -1,7 +1,6 @@
 const { bootcamp } = require("../models");
 const bootCampService = {
   createBootCamp: async data => {
-    console.log(data);
     const bootcampModel = {
       name: data.name,
       description: data.description,
@@ -13,6 +12,25 @@ const bootCampService = {
     };
     const bootcampCreate = await bootcamp.create(bootcampModel);
     return bootcampCreate;
+  },
+  getAllBootCamp: async () => {
+    return await bootcamp.findAll();
+  },
+  getBootCampById: async id => {
+    return await bootcamp.findByPk(id);
+  },
+  updateBootCamp: async (id, data) => {
+    console.log(id, data);
+    const updatedId = await bootcamp.update(
+      { name: data.name },
+      { where: { id: id } }
+      //{ where: { id: id }, returning: true, plain: true } it only work with postgres
+    );
+    return await bootcamp.findByPk(updatedId[0]);
+  },
+
+  deleteBootCamp: async id => {
+    return await bootcamp.destroy({ where: { id: id } });
   }
 };
 
